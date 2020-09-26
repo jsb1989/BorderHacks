88c8782573ae7067190f6200d90cae77308bdef7
FarazNaseem <faraz.n019@outlook.com>
2020-09-25 23:41:40 -0400

    Now, when the user enters the website, a homepage will be shown prompting the user to enter information, determining what the price of their airplane ticket is.

--- flaskmain.py
+++ flaskmain.py
@@ -2,5 +2,11 @@ from flask import Flask, render_template, request, url_for #Imports the flask fr
 
 app = Flask(__name__)
 
+
+@app.route('/')
+@app.route('/home')
+def homepage():
+    return render_template('webpage.html')
+
 if __name__ == 'main':
     app.run(debug=True)
