cb6c1f76a38b5cd630142e0f0a655c902ef43ac3
FarazNaseem <faraz.n019@outlook.com>
2020-09-26 15:24:24 -0400

    Applied css to the home page.

--- forms.py
+++ forms.py
@@ -4,8 +4,8 @@ from wtforms.validators import DataRequired
 
 
 class TicketForm(FlaskForm):
-    current_city = StringField("Enter name of the city you will be leaving from", )
-    destination_location = StringField("Enter the name of the city you want to go to",)
-    leave = StringField("When do you plan on leaving: yy-mm",)
-    returning = StringField("When do you plan on returning: yy-mm", )
+    current_city = StringField("Enter name of the city you will be leaving from", render_kw={"placeholder": "text"})
+    destination_location = StringField("Enter the name of the city you want to go to", render_kw={"placeholder": "text"})
+    leave = StringField("When do you plan on leaving: yy-mm", render_kw={"placeholder": "text"})
+    returning = StringField("When do you plan on returning: yy-mm", render_kw={"placeholder": "text"})
     submit_button = SubmitField("Find Tickets For Me")
