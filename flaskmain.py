c09d450d67794c4d9252b54a82008e35e8522cd9
FarazNaseem <faraz.n019@outlook.com>
2020-09-26 01:21:42 -0400

    Created an __init__.py file so we can import packages such as, forms.py. Also, I worked on the webpage.html file, by adding in some Jinja tags. We now have proper input paramters. However, they don't look that good. We have to make those better using Bootstrap, next.

--- flaskmain.py
+++ flaskmain.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template, request, url_for #Imports the flask framework.
+from flask import Flask, render_template, request, url_for # Imports the flask framework.
+from flask_bootstrap import Bootstrap
+from . forms import TicketForm
 
 app = Flask(__name__)
+app.secret_key = "SECRET"
+Bootstrap(app)
 
-
-@app.route('/')
-@app.route('/home')
+@app.route('/', methods=("GET", "POST"))
+@app.route('/home', methods=("GET", "POST"))
 def homepage():
-    return render_template('webpage.html')
+    ticketform = TicketForm()
+    return render_template('webpage.html', form=ticketform)
+
 
 if __name__ == 'main':
     app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import InputRequired
+from wtforms.validators import DataRequired
 
 
-class ticket_form(FlaskForm):
-    current_city = StringField("Enter name of the city you will be leaving from", InputRequired())
-    destination_location = StringField("Enter the name of the city you want to go to", InputRequired())
-    leave = StringField("When do you plan on leaving: yy-mm", InputRequired())
-    returning = StringField("When do you plan on returning: yy-mm", InputRequired())
+class TicketForm(FlaskForm):
+    current_city = StringField("Enter name of the city you will be leaving from", )
+    destination_location = StringField("Enter the name of the city you want to go to",)
+    leave = StringField("When do you plan on leaving: yy-mm",)
+    returning = StringField("When do you plan on returning: yy-mm", )
     submit_button = SubmitField("Find Tickets For Me")
