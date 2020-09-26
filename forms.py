34e37fb43356a23ea691d166ffde9e926d5b0d56
FarazNaseem <faraz.n019@outlook.com>
2020-09-26 17:37:56 -0400

    Added the fonts to the home page, and now I also have placeholders instead of labels for the form inputs.

--- forms.py
+++ forms.py
@@ -4,8 +4,8 @@ from wtforms.validators import DataRequired
 
 
 class TicketForm(FlaskForm):
-    current_city = StringField("Enter name of the city you will be leaving from", render_kw={"placeholder": "text"})
-    destination_location = StringField("Enter the name of the city you want to go to", render_kw={"placeholder": "text"})
-    leave = StringField("When do you plan on leaving: yy-mm", render_kw={"placeholder": "text"})
-    returning = StringField("When do you plan on returning: yy-mm", render_kw={"placeholder": "text"})
+    current_city = StringField("Enter name of the city you will be leaving from", render_kw={"placeholder": "Enter An Origin"})
+    destination_location = StringField("Enter the name of the city you want to go to", render_kw={"placeholder": "Enter A Destination"})
+    leave = StringField("When do you plan on leaving: yy-mm", render_kw={"placeholder": "Start Date (yy-mm)"})
+    returning = StringField("When do you plan on returning: yy-mm", render_kw={"placeholder": "Return Date (yy-mm)"})
     submit_button = SubmitField("Find Tickets For Me")
